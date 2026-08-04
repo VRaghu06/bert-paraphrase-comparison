9795d3630057bf8396acb85adc6115c339cf4906
Vedhanth Raghu <vedhanthraghu@gmail.com>
2026-08-04 01:41:43 -0400

    Fixed the tester files and updated the .gitignore file

--- paraphrase-tester-fineTuned.py
+++ paraphrase-tester-fineTuned.py
@@ -1,7 +1,50 @@
-from transformers import pipeline
+from datasets import load_dataset
+from transformers import(
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer,
+)
 
-classifier = pipeline("text-classification" , model = "fine-tuning-trial1" , tokenizer = "fine-tuning-trial1", device=0)
+import evaluate
+import numpy as np
 
-result = classifier([{"text":"The company reported strong earnings." , "text_pair":"The firm posted strong profits."}, {"text":"She did not attend the party." , "text_pair":"The party was not graced with her presence."}])
+raw_dataset = load_dataset("glue" , "mrpc")
 
-print(result)
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+def tokenizing_func(main_dict):
+    return tokenizer(main_dict["sentence1"], main_dict["sentence2"] , truncation = True)
+
+tokenized_dataset = raw_dataset.map(tokenizing_func , batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+metric = evaluate.load("glue" , "mrpc")
+
+def compute_metrics(eval_preds):
+    logits,labels = eval_preds
+    predictions = np.argmax(logits , axis=-1)
+    return metric.compute(predictions=predictions ,  references=labels)
+
+fineTuned_model = AutoModelForSequenceClassification.from_pretrained("fine-tuning-trial1")
+
+eval_args = TrainingArguments(
+    output_dir="eval-temp-fineTuned",
+    per_device_eval_batch_size=8,
+    fp16=True,
+)
+
+trainer=Trainer(
+    model = fineTuned_model,
+    args=eval_args,
+    eval_dataset = tokenized_dataset["validation"],
+    data_collator=data_collator,
+    processing_class = tokenizer,
+    compute_metrics = compute_metrics,
+)
+
+results = trainer.evaluate()
+print(f"Accuracy: {results['eval_accuracy']:.4f}")
+print(f"F1: {results['eval_f1']:.4f}")
--- paraphrase-tester-preTrained.py
+++ paraphrase-tester-preTrained.py
@@ -1,7 +1,50 @@
-from transformers import pipeline
+from datasets import load_dataset
+from transformers import(
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer,
+)
 
-classifier = pipeline("text-classification" , model = "bert-base-uncased" , device=0)
+import evaluate
+import numpy as np
 
-result = classifier([{"text":"The company reported strong earnings." , "text_pair":"The firm posted strong profits."}, {"text":"She did not attend the party." , "text_pair":"The party was not graced with her presence."}])
+raw_dataset = load_dataset("glue" , "mrpc")
 
-print(result)
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+def tokenizing_func(main_dict):
+    return tokenizer(main_dict["sentence1"] , main_dict["sentence2"] , truncation = True)
+
+tokenized_dataset = raw_dataset.map(tokenizing_func , batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+metric = evaluate.load("glue" , "mrpc")
+
+def compute_metrics(eval_preds):
+    logits,labels = eval_preds
+    predictions = np.argmax(logits , axis=-1)
+    return metric.compute(predictions = predictions , references=labels)
+
+base_model = AutoModelForSequenceClassification.from_pretrained(checkpoint , num_labels = 2)#since the model has not been trained on this type of data, we got to attach this randomly initialized classification head
+
+eval_args = TrainingArguments(
+    output_dir="eval-temp-base",
+    per_device_eval_batch_size=8,
+    fp16=True,
+)
+
+trainer = Trainer(
+    model=base_model,
+    args = eval_args,
+    eval_dataset = tokenized_dataset["validation"],
+    data_collator = data_collator,
+    processing_class = tokenizer,
+    compute_metrics = compute_metrics,
+)
+
+results = trainer.evaluate()
+print(f"Accuracy: {results['eval_accuracy']:.4f}")
+print(f"F1 : {results['eval_f1']:.4f}")
